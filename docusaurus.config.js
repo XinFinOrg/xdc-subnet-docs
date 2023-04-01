0a7aa88889c12649a89715adb648399c5e100dad
Liam <liam.icheng.lai@gmail.com>
2023-04-01 19:32:23 +1100

    replace landing page with docs page (#9)
    * use doc as main page
    
    * use doc as main page
    
    * use doc as main page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,20 +29,15 @@ const config = {
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/XinFinOrg/XDC-Subnet/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
 
